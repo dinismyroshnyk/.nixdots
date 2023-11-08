69eca3cf2d2b374ad661f6a9f5189c3324351899
Dinis Myroshnyk <dinismyroshnyk2@gmail.com>
2023-11-08 15:23:49 +0000

    Small changes.

--- modules/programs/lf/default.nix
+++ modules/programs/lf/default.nix
@@ -7,8 +7,15 @@ let
 in {
     options.modules.lf = { enable = mkEnableOption "lf"; };
     config = mkIf cfg.enable {
+        xdg.configFile."lf/icons".source = ./icons;
         programs.lf = {
             enable = true;
+            settings = {
+                preview = true;
+                hidden = true;
+                drawbox = true;
+                icons = true;
+            };
         };
     };
 }
