3c571be362be69b2cd6f8325a9410d5a16877c5b
Dinis Myroshnyk <dinismyroshnyk2@gmail.com>
2024-03-08 11:23:28 +0000

    Add NVIDIA power management and offload support

--- modules/system/nvidia.nix
+++ modules/system/nvidia.nix
@@ -2,7 +2,23 @@
 
 {
     # Enable NVIDIA drivers.
-    hardware.nvidia.modesetting.enable = true;
+    hardware.nvidia = {
+        modesetting.enable = true;
+        powerManagement = {
+            enable = true;
+            finegrained = true;
+        };
+        prime = {
+            amdgpuBusId = "PCI:7:0:0";
+            nvidiaBusId = "PCI:1:0:0";
+            offload = {
+                enable = true;
+                enableOffloadCmd = true;
+            };
+        };
+        open = true;
+        nvidiaSettings = true;
+    };
     services.xserver.videoDrivers = [ "nvidia" ];
 
     # Enable OpenGL
