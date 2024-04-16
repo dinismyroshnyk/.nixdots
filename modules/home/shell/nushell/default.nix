df3cea99b60106c22c4594afb5dabb1749e9dc3c
Dinis Myroshnyk <dinismyroshnyk2@gmail.com>
2024-04-16 23:20:18 +0100

    Working aliases

--- modules/home/shell/nushell/default.nix
+++ modules/home/shell/nushell/default.nix
@@ -13,46 +13,41 @@ in {
                 enable = true;
                 extraConfig = ''
                     let carapace_completer = {|spans|
-                    carapace $spans.0 nushell $spans | from json
+                        carapace $spans.0 nushell $spans | from json
                     }
                     $env.config = {
                         show_banner: false,
                         completions: {
-                        case_sensitive: false # case-sensitive completions
-                        quick: true    # set to false to prevent auto-selecting completions
-                        partial: true    # set to false to prevent partial filling of the prompt
-                        algorithm: "fuzzy"    # prefix or fuzzy
-                        external: {
-                        # set to false to prevent nushell looking into $env.PATH to find more suggestions
-                            enable: true
-                        # set to lower can improve completion performance at the cost of omitting some options
-                            max_results: 100
-                            completer: $carapace_completer # check 'carapace_completer'
-                        }
+                            case_sensitive: false # case-sensitive completions
+                            quick: true    # set to false to prevent auto-selecting completions
+                            partial: true    # set to false to prevent partial filling of the prompt
+                            algorithm: "fuzzy"    # prefix or fuzzy
+                            external: {
+                                # set to false to prevent nushell looking into $env.PATH to find more suggestions
+                                enable: true
+                                # set to lower can improve completion performance at the cost of omitting some options
+                                max_results: 100
+                                completer: $carapace_completer # check 'carapace_completer'
+                            }
                         }
                     }
+                    let home_apps = $env.HOME + ".apps";
                     $env.PATH = ($env.PATH |
-                    split row (char esep) |
-                    prepend /home/myuser/.apps |
-                    append /usr/bin/env
+                        split row (char esep) |
+                        prepend $home_apps |
+                        append /usr/bin/env
                     )
 
-                    # Shell custom definitions (complex aliases)
-                    def update [] {
-                        let nixos_config_dir = $env.NIXOS_CONFIG_DIR
-                        nix flake update $nixos_config_dir
-                    }
-                    def rebuild [] {
-                        let nixos_config_dir = $env.NIXOS_CONFIG_DIR
-                        let hostname = $env.HOSTNAME
-                        let flake = $nixos_config_dir + "#" + $hostname
-                        sudo nixos-rebuild switch --flake $flake
-                    }
+                    # Shell aliases (easier to configure here than in shellAliases option)
+                    let nixos_config_dir = $env.NIXOS_CONFIG_DIR;
+                    let hostname = $env.HOSTNAME;
+                    let flake = $nixos_config_dir + "#" + $hostname;
+
+                    alias cls = clear;
+                    alias rebuild = sudo nixos-rebuild switch --flake $flake;
+                    alias update = nix flake update $nixos_config_dir;
+                    alias upgrade = update;rebuild;
                 '';
-                shellAliases = {
-                    cls = "clear";
-                    upgrade = "update;rebuild";
-                };
             };
             carapace = {
                 enable = true;
